637f8718ef9470fba033bbc20187e188beb38b5b
Colburn Sanders <colburnsanders@gmail.com>
2019-04-27 18:00:17 -0500

    testing hash method

--- dev/test.js
+++ dev/test.js
@@ -1,18 +1,44 @@
 //jshint esversion: 6
 const Blockchain = require('./blockchain'); // aquiring the blockchain js file.
 
-const bitcoin = new Blockchain();
-bitcoin.createNewBlock(2389, 'dkfjaldkfjldkfj', 'dkfjadlfkjadfk');
+// const bitcoin = new Blockchain();
+// bitcoin.createNewBlock(2389, 'dkfjaldkfjldkfj', 'dkfjadlfkjadfk');
 
-bitcoin.createNewTransaction(13, 'COLBURN123456', 'SEMSA1234567');
+// bitcoin.createNewTransaction(13, 'COLBURN123456', 'SEMSA1234567');
 
-bitcoin.createNewBlock(2390, 'dkfjadkfjaldfjf', 'dkfjadlfkjadfk'); // mine block
+// bitcoin.createNewBlock(2390, 'dkfjadkfjaldfjf', 'dkfjadlfkjadfk'); // mine block
 
-bitcoin.createNewTransaction(50, 'COLBURN123456', 'SEMSA1234567');
-bitcoin.createNewTransaction(300, 'COLBURN123456', 'SEMSA1234567');
-bitcoin.createNewTransaction(2000, 'COLBURN123456', 'SEMSA1234567');
+// bitcoin.createNewTransaction(50, 'COLBURN123456', 'SEMSA1234567');
+// bitcoin.createNewTransaction(300, 'COLBURN123456', 'SEMSA1234567');
+// bitcoin.createNewTransaction(2000, 'COLBURN123456', 'SEMSA1234567');
 
-bitcoin.createNewBlock(2400, 'dkfjadkfjaldfjf', 'dkfjadlfkjadfk'); // mine block
+// bitcoin.createNewBlock(2400, 'dkfjadkfjaldfjf', 'dkfjadlfkjadfk'); // mine block
 
-console.log(bitcoin.chain[2]);
+
+// Testing Hash Method
+const blockChain = new Blockchain();
+
+const previousHashBlock = '51654646465421dfdf516';
+const currentBlockData = [
+    {
+        amount: 100,
+        sender: 'COLBURN1234567',
+        recipient: 'SEMSA12345678',
+    },
+    {
+        amount: 200,
+        sender: 'COLBURN987456',
+        recipient: 'SEMSA987456',
+    },
+    {
+        amount: 300,
+        sender: 'COLBURN123789',
+        recipient: 'SEMSA123789',
+    }
+];
+const nonce = 75;
+
+blockChain.hashBlock(previousHashBlock, currentBlockData, nonce);
+
+console.log(blockChain.hashBlock(previousHashBlock, currentBlockData, nonce));
 
